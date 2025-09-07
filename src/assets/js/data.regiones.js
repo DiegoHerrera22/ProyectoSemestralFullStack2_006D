9b19524871c22607d23b325cfa47b61ba9f583e2
Joaquín Rodrigo Araya Monsalve <joa.arayam@duocuc.cl>
2025-09-07 20:21:20 -0300

    ep2

--- src/assets/js/data.regiones.js
+++ src/assets/js/data.regiones.js
@@ -1,6 +1,69 @@
 
-// Ejemplo mínimo. Agrega todas las regiones/comunas según pauta.
+// Lista de regiones y comunas utilizadas
 window.REGIONES = {
-  "Metropolitana de Santiago": ["La Florida","Puente Alto","Providencia","Santiago Centro"],
-  "Valparaíso": ["Valparaíso","Viña del Mar"]
+  "Tarapacá": [
+    "Alto Hospicio",
+    "Iquique",
+    "Pica",
+    "Pozo Almonte"
+  ],
+  "Coquimbo": [
+    "Andacollo",
+    "Coquimbo",
+    "La Serena",
+    "Ovalle"
+  ],
+  "Valparaíso": [
+    "Quilpué",
+    "San Antonio",
+    "Valparaíso",
+    "Viña del Mar",
+    "Villa Alemana"
+  ],
+  "Metropolitana de Santiago": [
+    "Colina",
+    "Huechuraba",
+    "La Florida",
+    "La Reina",
+    "Las Condes",
+    "Lo Barnechea",
+    "Macul",
+    "Maipú",
+    "Ñuñoa",
+    "Providencia",
+    "Puente Alto",
+    "San Joaquín",
+    "San Miguel",
+    "Santiago Centro",
+    "Vitacura"
+  ],
+  "O'Higgins": [
+    "Machalí",
+    "Rancagua",
+    "San Fernando"
+  ],
+  "Maule": [
+    "Constitución",
+    "Curicó",
+    "Linares",
+    "Talca"
+  ],
+  "Biobío": [
+    "Concepción",
+    "Hualpén",
+    "Los Ángeles",
+    "San Pedro de la Paz",
+    "Talcahuano"
+  ],
+  "La Araucanía": [
+    "Padre Las Casas",
+    "Pucón",
+    "Temuco",
+    "Villarrica"
+  ],
+  "Los Ríos": [
+    "La Unión",
+    "Río Bueno",
+    "Valdivia"
+  ]
 };
